dfc5750f4dcebd3dab6c9f8d224114795be0fc3f
Mark Watkinson <markwatkinson@gmail.com>
2012-06-29 23:17:10 +0100

    Implement playlist sorting

--- static/lib/viewmodels/PlaylistViewModel.js
+++ static/lib/viewmodels/PlaylistViewModel.js
@@ -1,7 +1,60 @@
 "use strict";
 window.PlaylistViewModel = function() {
     var self = this, 
-        audioElement = document.getElementById('current-audio');
+        audioElement = document.getElementById('current-audio'),
+        comparators = {
+            // these all operate on song objects apart from cmp which operates
+            // on anything the < and > operators are defined for, so it's
+            // basically a primtive cmp
+            cmp : function(a, b) {
+                if (a < b) return -1;
+                if (a > b) return 1;
+                return 0;
+            },
+            trackNo: function(a, b) {
+                return comparators.cmp(a.trackNo(), b.trackNo())
+            },
+            title: function(a, b) {
+                return comparators.cmp(a.title(), b.title());
+            },
+            
+            
+            artist: function(a, b) {
+                // artist calls album
+                var c = comparators.cmp(a.album.artist.name(),
+                                        b.album.artist.name())
+                if (c == 0) {
+                    c = comparators.album(a, b)
+                }
+                return c;
+            },
+            album: function(a, b) {
+                // album calls trackNo
+                var c = comparators.cmp(a.album.title(),
+                                        b.album.title());
+                if (c == 0) {
+                    c = comparators.trackNo(a, b);
+                }
+                return c;
+            },
+            year: function(a, b) {
+                // year calls artist
+                var c = comparators.cmp(a.album.year(), b.album.year());
+                if (c == 0) {
+                    c = comparators.artist(a, b);
+                }
+                return c;
+            },
+            length: function(a, b) {
+                var c = comparators.cmp(a.length(), b.length());
+                if (c == 0) {
+                    c = comparators.artist(a, b);
+                }
+                return c;
+            }
+            
+        },
+        ignoreIndex = false;
 
     this.songs = ko.observableArray([]);
     this.random = ko.observable(false);
@@ -24,7 +77,10 @@ window.PlaylistViewModel = function() {
     
     
     this.index.subscribe(function() {
-        var song = this.songs()[this.index()];
+        var song;
+        if (ignoreIndex) 
+            return;
+        song = this.songs()[this.index()];
         if (!song) {
             this.playing(false);
             return;
@@ -41,6 +97,7 @@ window.PlaylistViewModel = function() {
     
     this.addToPlaylist = function(obj, autoPlay) {
         var index = 0;
+        
         if (typeof autoPlay === 'undefined') autoPlay = true;
         if (obj instanceof SongModel) {
             self.songs.push(obj);
@@ -54,6 +111,7 @@ window.PlaylistViewModel = function() {
                 self.addToPlaylist(e, false);
             });
         }
+        else { console.log('what is obj?') }
         if (autoPlay && !this.playing()) {
             this.playIndex(index);
         }
@@ -85,6 +143,27 @@ window.PlaylistViewModel = function() {
     }.bind(this);
     
     
+    
+    this.sortBy = function(field) {
+        var sortCmp = comparators[field], currentSong = this.currentSong();
+        if (sortCmp) {
+            self.songs.sort(sortCmp);
+        }
+        // now we've just changed the order, so we need to make sure
+        // the index is updated to match the current playing song
+        if (currentSong) {
+            music.utils.each(this.songs(), function(i, e) {
+                if (e == currentSong) {
+                    ignoreIndex = true;
+                    self.index(i);
+                    ignoreIndex = false
+                    return true;
+                }
+            });
+        }
+    }.bind(this);
+    
+    
     audioElement.addEventListener('ended', function() {
         console.log('ended playback');
         self.next();
@@ -92,6 +171,7 @@ window.PlaylistViewModel = function() {
     audioElement.addEventListener('durationchange', function() {
         
     }, false);
+    
     
     
 }
